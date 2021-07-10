fb24282d4dac093dc3c084912bfc673c1773ae7d
Shreyas180 <77526596+Shreyas180@users.noreply.github.com>
2021-07-10 21:24:48 +0530

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,18 @@
+class Particle
+{
+constructor()
+{
+this.body = Bodies.circle(x,y, this.r,options);
+this.color=color(random(0,255), random(0,255), random(0,World.add(world,this.body)));
+if(frameElement%60===0)
+{
+    particles.push(new Particle(random(width/2-10, width2+10), 10,10));
+}
+}
+display()
+{
+    var particles = []
+    var plinkos = []
+    var divisions = []
+}
+}
